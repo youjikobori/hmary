c30289c218f01304705aa53f861d0fb403391828
youjikobori <youjikobori@tutamail.com>
2026-02-16 15:56:24 +0600

    Update version_checker.py

--- hmary/version_checker.py
+++ hmary/version_checker.py
@@ -1,27 +1,27 @@
 import requests
 import webbrowser
-import tomllib  # для Python 3.11+
+import tomllib  # Python 3.11+
 
 def check_version():
-    # Текущая версия из pyproject.toml
+    # pyproject.toml
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
         current = data["project"]["version"]
     
-    print(f"Добро пожаловать, версия текущего проекта: {current}")
+    print(f" Welcome!\n Current version of the project: {current}")
     
-    # Проверка GitHub
-    try:
-        r = requests.get("https://api.github.com/repos/VLRZZ/hmary/releases/latest", timeout=5)
+    # GitHub-Api Checker
+    try:a
+        r = requests.get("https://api.github.com/repos/youjikobori/hmary/releases/latest", timeout=5)
         if r.status_code == 200:
             latest = r.json()["tag_name"]
             if latest != current:
-                print(f"Доступно обновление на версию {latest}")
-                if input("Хотите обновится (Y/n): ").lower() == 'y':
+                print(f" Update to version {latest} is available.")
+                if input(" Would you like to update? (Y/n): ").lower() == 'y':
                     webbrowser.open(r.json()["html_url"])
             else:
-                input("У вас самая последняя версия программы\nнажмите [ENTER] чтобы продолжить")
+                input(" You have the latest version of the program\n Press [ENTER] to continue")
         else:
-            input("Не удалось проверить обновления\nнажмите [ENTER] чтобы продолжить")
+            input(" Unable to check for updates\n Press [ENTER] to continue")
     except:
-        input("Не удалось проверить обновления\nнажмите [ENTER] чтобы продолжить")
+        input(" Unable to check for updates\n Press [ENTER] to continue")
